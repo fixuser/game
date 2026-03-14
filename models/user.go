8cebc42fd52cd983b98afdffc755ec3453de58b8
test <test@gmail.com>
2026-03-14 10:46:49 +0800

    完善用户模型

--- models/user.go
+++ models/user.go
@@ -16,27 +16,28 @@ import (
 
 // User 用户
 type User struct {
-	Id           int64              `bun:",pk"`
-	InviteIds    []int64            `bun:",array,nullzero"`        // 邀请关系
-	Type         basepb.User_Type   `bun:",notnull,type:smallint"` // 用户类型
-	Nickname     string             `bun:",nullzero"`              // 昵称
-	Avatar       string             `bun:",nullzero"`              // 头像
-	Gender       basepb.User_Gender `bun:",notnull,type:smallint"` // 性别
-	Email        string             `bun:",nullzero"`              // 邮箱
-	AreaCode     int16              `bun:",notnull,type:smallint"` // 地区 86,1,852
-	Phone        string             `bun:",nullzero"`              // 电话
-	VerifiedTags tag.Tag            `bun:",nullzero,type:bigint"`  // 验证情况
-	Password     *Argon2Data        `bun:",nullzero,type:jsonb"`   // 密码
-	Point        decimal.Decimal    `bun:",notnull,type:decimal"`  // 积分
-	TotalPoint   decimal.Decimal    `bun:",notnull,type:decimal"`  // 累计积分
-	Ip           string             `bun:",nullzero"`              // 注册时候的IP地址
-	DeviceId     string             `bun:",nullzero"`              // 注册时候的设备ID
-	Tags         tag.Tag            `bun:",nullzero,type:bigint"`  // 标签
-	Status       basepb.User_Status `bun:",notnull,type:smallint"` // 状态
-	Extras       json.RawMessage    `bun:",nullzero,type:jsonb"`   // 扩展信息
-	TerminatedAt *time.Time         `bun:",nullzero"`              // 终止时间
-	CreatedAt    time.Time          `bun:",nullzero,notnull,default:current_timestamp"`
-	UpdatedAt    time.Time          `bun:",nullzero,notnull,default:current_timestamp"`
+	Id           int64               `bun:",pk"`
+	InviteIds    []int64             `bun:",array,nullzero"`        // 邀请关系
+	Type         basepb.User_Type    `bun:",notnull,type:smallint"` // 用户类型
+	Nickname     string              `bun:",nullzero"`              // 昵称
+	Avatar       string              `bun:",nullzero"`              // 头像
+	Gender       basepb.User_Gender  `bun:",notnull,type:smallint"` // 性别
+	RegType      basepb.User_RegType `bun:",notnull,type:smallint"` // 注册类型
+	Email        string              `bun:",nullzero"`              // 邮箱
+	AreaCode     int16               `bun:",notnull,type:smallint"` // 地区 86,1,852
+	Phone        string              `bun:",nullzero"`              // 电话
+	VerifiedTags tag.Tag             `bun:",nullzero,type:bigint"`  // 验证情况
+	Password     *Argon2Data         `bun:",nullzero,type:jsonb"`   // 密码
+	Point        decimal.Decimal     `bun:",notnull,type:decimal"`  // 积分
+	TotalPoint   decimal.Decimal     `bun:",notnull,type:decimal"`  // 累计积分
+	Ip           string              `bun:",nullzero"`              // 注册时候的IP地址
+	DeviceId     string              `bun:",nullzero"`              // 注册时候的设备ID
+	Tags         tag.Tag             `bun:",nullzero,type:bigint"`  // 标签
+	Status       basepb.User_Status  `bun:",notnull,type:smallint"` // 状态
+	Extras       json.RawMessage     `bun:",nullzero,type:jsonb"`   // 扩展信息
+	TerminatedAt *time.Time          `bun:",nullzero"`              // 终止时间
+	CreatedAt    time.Time           `bun:",nullzero,notnull,default:current_timestamp"`
+	UpdatedAt    time.Time           `bun:",nullzero,notnull,default:current_timestamp"`
 }
 
 // GenUserId 生成用户ID
