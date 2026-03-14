2eb38814a4704a1167618d0faa80f181d7ed7eed
test <test@gmail.com>
2026-03-14 10:08:46 +0800

    优化scan

--- internal/utils/rows.go
+++ internal/utils/rows.go
@@ -7,17 +7,19 @@ import (
 	"github.com/uptrace/bun"
 )
 
-// Scan 遍历查询结果并应用回调函数
-func Scan[T any](db *bun.DB, rows *sql.Rows, fn func(dst *T)) (err error) {
-	ctx := context.Background()
-	var t T
+// Scan 逐行遍历查询结果并应用回调函数，避免一次性加载大量数据占用过多内存。
+// T 应该是一个指针类型，例如 *User。
+func Scan[T any](ctx context.Context, db *bun.DB, rows *sql.Rows, fn func(dst T) error) error {
+	defer rows.Close()
 	for rows.Next() {
-		err = db.ScanRow(ctx, rows, &t)
-		if err != nil {
-			return
+		// New 实例化泛型指针所指向的具体类型
+		var t T
+		if err := db.ScanRow(ctx, rows, &t); err != nil {
+			return err
 		}
-		fn(&t)
+		if err := fn(t); err != nil {
+			return err
+		}
 	}
-	err = rows.Err()
-	return
+	return rows.Err()
 }
